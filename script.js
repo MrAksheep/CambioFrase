abb6ee0b2cb72899e12002d5a7ec77232a54e281
MrAksheep <pablovera.pvba@gmail.com>
2024-01-21 18:53:28 -0300

    chabale

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const texto = document.getElementById('pablo')
 
 let texto1 = 'Hola querido amigo!'
 let texto2 = 'pico pal que lee!'
+let viernes = '<iframe width="560" height="315" src="https://www.youtube.com/embed/aSUuw5HY9eY?si=Yc6BEDG2dUgkKfQM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
 
 btn.addEventListener('click', () => {
-    texto.innerText = texto2
+    texto.innerHTML = viernes
 })

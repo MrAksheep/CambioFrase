9296e35ee874b6c6b5352e8a91bfaa30ba0d868c
Miguel Medina <mamedina770@gmail.com>
2024-01-22 11:13:52 -0300

    mute

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const texto = document.getElementById("pablo");
 let texto1 = "Hola querido amigo!";
 let texto2 = "pico pal que lee!";
 let viernes =
-  '<iframe width="560" height="315" src="https://www.youtube.com/embed/aSUuw5HY9eY?si=2wi3BN1xjD8PRlKS&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+  '<iframe width="560" height="315" src="https://www.youtube.com/embed/aSUuw5HY9eY?si=2wi3BN1xjD8PRlKS&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
 
 btn.addEventListener("click", () => {
   texto.innerHTML = viernes;

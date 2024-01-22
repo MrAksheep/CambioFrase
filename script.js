3cf292cb5d528a346d48b47c598fa4eee903a3f5
Miguel Medina <mamedina770@gmail.com>
2024-01-22 11:33:24 -0300

    cambios esteticos de la pagina

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const btn = document.getElementById("boton");
 const texto = document.getElementById("pablo");
-
+const main = document.getElementsByTagName("main");
+console.log(main);
 let texto1 = "Hola querido amigo!";
 let texto2 = "pico pal que lee!";
 let viernes = "<video autoplay src='viernes.mp4'></video>";
 
 btn.addEventListener("click", () => {
-  texto.innerHTML = viernes;
+  main[0].innerHTML = viernes;
 });
